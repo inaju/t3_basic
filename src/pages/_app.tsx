087077664ebcb710768ca5b248f4051556beeb53
[mitchel inaju] <[mitchelinajuo@gmail.com]>
2023-06-03 03:51:08 +0100

    added clerk and removed nextauth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,16 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
+    <ClerkProvider>
       <Component {...pageProps} />
-    </SessionProvider>
+    </ClerkProvider>
   );
 };
 
